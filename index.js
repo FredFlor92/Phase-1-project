924c8ac8151eacc74b2d703713ed7d2862dd0bfc
Freddy Florial <freddyflorial@gmail.com>
2022-10-02 14:23:11 -0400

    templates

--- index.js
+++ index.js
@@ -11,3 +11,29 @@ const breakfastInput = () => document.getElementById('breakfast');
 const lunchInput = () => document.getElementById('lunch');
 const dinnerInput = () => document.getElementById('dinner');
 const stepsInput = () => document.getElementById('steps');
+
+//Meals
+
+const foodStepsItemsTemplate = ({date, breakfast, lunch, dinner, steps}) => {
+
+    const tr = document.createElement('tr');
+    const tdDate = document.createElement('td');
+    const tdBreakfast = document.createElement('td');
+    const tdLunch = document.createElement('td');
+    const tdDinner = document.createElement('td');
+    const tdSteps = document.createElement('td');
+
+    tdDate.innerText = date;
+    tdBreakfast.innerText = breakfast;
+    tdLunch.innerText = lunch;
+    tdDinner.innerText = dinner;
+    tdSteps.innerText = steps;
+
+    tr.appendChild(tdDate);
+    tr.appendChild(tdBreakfast);
+    tr.appendChild(tdLunch);
+    tr.appendChild(tdDinner);
+    tr.appendChild(tdSteps);
+
+    return tr;
+}
