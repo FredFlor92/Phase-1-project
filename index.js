138f73f396a28e92c6e994ee879ff4b3100a6f5c
Freddy Florial <freddyflorial@gmail.com>
2022-10-02 14:15:00 -0400

    ID getters

---
+++ index.js
@@ -0,0 +1,13 @@
+let foodStepsItems = [];
+
+//ID Get
+
+const mainDiv = () => document.getElementById"main");
+const homePageLink = () => document.getElementById("home-page-lnk");
+const loggedFoodStepsLink = () => document.getElementById("logged-food-steps-link");
+const newEntryFormLink = () => document.getElementById("new-entry-form-link");
+const dateInput = () => document.getElementById('date');
+const breakfastInput = () => document.getElementById('breakfast');
+const lunchInput = () => document.getElementById('lunch');
+const dinnerInput = () => document.getElementById('dinner');
+const stepsInput = () => document.getElementById('steps');
