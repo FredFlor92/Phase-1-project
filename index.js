4d5e6e1e791ba4f3533d810792087f2678f75334
Freddy Florial <freddyflorial@gmail.com>
2022-10-03 20:09:48 -0400

    page up and running

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-let warriorArray = []
+let fighterArray = []
 
 document.addEventListener("DOMContentLoaded", () => {
-    alert("RNGesus will... CHOOSE YOUR WARRIOR!")
-    grabWarriors()
-    addWarriorsToArray()
+    alert("RNGesus will... CHOOSE YOUR FIGHTER!!")
+    grabFighters()
+    addFightersToArray()
     document.querySelector("#reset").addEventListener("click", () => {
         document.getElementById("character-select").innerHTML = "";
-        warriorArray.length = 0
-        grabWarriors()
-        addWarriorsToArray()
+        fighterArray.length = 0
+        grabFighters()
+        addFightersToArray()
         document.getElementById("selected-character").innerHTML = ""
     })
-
-    document.querySelector("randomize").addEventListener("click", () => {
-        removeRandom(warriorArray)
+    
+    document.querySelector("#randomize").addEventListener("click", () => {
+        removeRandom(fighterArray)
     })
-
+    
     document.getElementById("theme-dropdown").addEventListener("change", (e) => {
         if (e.target.value === "dark") {
             document.querySelector("link[href='light.css']").href = "dark.css";
@@ -23,32 +23,34 @@ document.addEventListener("DOMContentLoaded", () => {
         else {
             document.querySelector("link[href='dark.css']").href = "light.css"
         }
-    })
-})
+        }
+    )
+    }    
+);
 
-function grabWarriors() {
-    fetch("http://localhost:3000/warriors")
+function grabFighters() {
+    fetch("http://localhost:3000/fighters")
     .then(response => response.json())
-    .then(data => data.forEach(warrior => showWarriors(warrior)))
+    .then(data => data.forEach(fighter => showFighters(fighter)))
 };
 
-function addWarriorsToArray() {
-    fetch("http://localhost:3000/warriorss")
+function addFightersToArray() {
+    fetch("http://localhost:3000/fighters")
     .then(response => response.json())
-    .then(data => data.forEach(warrior => warriorArray.push(warrior.name)))
+    .then(data => data.forEach(fighter => fighterArray.push(fighter.name)))
 }; 
 
-function showWarriorss(warrior) {
-    let warriorContainer = document.getElementById("character-select");
+function showFighters(fighter) {
+    let fighterContainer = document.getElementById("character-select");
     const div = document.createElement("div");
     div.classList.add("card");
     const h2 = document.createElement("h2");
-    h2.textContent = warrior.name;
-    h2.id = warrior.name;
+    h2.textContent = fighter.name;
+    h2.id = fighter.name;
     const img = document.createElement("img")
-    img.src = warrior.image;
+    img.src = fighter.image;
     div.append(h2, img);
-    warriorContainer.append(div);
+    fighterContainer.append(div);
 };
 
 const removeRandom = (array) => {
@@ -56,10 +58,10 @@ const removeRandom = (array) => {
     selectedContainer.innerHTML = ""
     if (array.length > 0) {
         const random = Math.floor(Math.random() * array.length);
-        const chosenWarrior = array.splice(random, 1)[0];
+        const chosenFighter = array.splice(random, 1)[0];
         const allCards = document.querySelectorAll('.card');
         for (const card of allCards) {
-            if (card.querySelector("h2").innerText === chosenWarrior) {
+            if (card.querySelector("h2").innerText === chosenFighter) {
                 card.remove();
                 selectedContainer.append(card);
             }
